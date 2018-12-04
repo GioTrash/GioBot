a9d8c6bdb1bf6dd9b4d9c75ba9359c292a2c5e31
GioTrash <45596494+GioTrash@users.noreply.github.com>
2018-12-04 15:38:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ fs.readdir("./commands", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setStatus("Gio code", {type: "WATCHING"});
+  client.user.setActivity("Gio code", {type: "WATCHING"});
 
 });
 
