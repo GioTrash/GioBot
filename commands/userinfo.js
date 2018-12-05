dee440d81af27f9227cbc570b03d17c480f3be35
GioTrash <45596494+GioTrash@users.noreply.github.com>
2018-12-05 09:49:42 +0100

    Add files via upload

---
+++ commands/userinfo.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const moment = require("moment");
+
+
+module.exports.run = async(bot, message, args) => {
+    let user;
+    if (message.mentions.user.first ()) {
+        user = message.mentions.users.first();
+    }else {
+        user = message.author;
+    }
+
+
+    const member = message.guild.member(user);
+
+    const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setThumbnail(message.author.avatarURL)
+    .setTitle(`${user.username}#${user.discriminator}`)
+    .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : "none"}`, true)
+    .addField("Created at", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
+    .addField("Bot", `${user.bot}`, true)
+    .addField("Status", `${user.presence.status}`, true)
+    .addField("Game", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+    .addField("Roles", member.roles.map(roles => `${roles.name}`). join(', '), true);
+    
+    message.channel.send({embed});
+}
+
+module.exports.help = {
+    name:"userinfo"
+}
