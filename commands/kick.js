95f4c3826ff393f167e784d327c3efa49bdf7b54
GioTrash <45596494+GioTrash@users.noreply.github.com>
2018-12-07 15:31:22 +0100

    Add files via upload

--- commands/kick.js
+++ commands/kick.js
@@ -13,13 +13,13 @@ module.exports.run = async (bot, message, args) => {
   if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
   const embed = new Discord.RichEmbed()
-  .setDescription("~Kick~")
-  .setColor("#e56b00")
-  .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-  .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-  .addField("Kicked In", message.channel)
-  .addField("Tiime", message.createdAt)
-  .addField("Reason", kReason);
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
 
   let kickChannel = message.guild.channels.find(`name`, "incidents");
   if(!kickChannel) return message.channel.send("Can't find incidents channel.");
