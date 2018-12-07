5638ae93749043e64655ac2d6d5d0c4b74290d45
GioTrash <45596494+GioTrash@users.noreply.github.com>
2018-12-07 10:47:39 +0100

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,7 @@ const Discord = require ("discord.js");
 module.exports.run = async(bot, message, args) => {
     const msg = await message.channel.send("ping?").then((msg)=>{
         msg.edit(`Pong! Latency is ${msg.createdTimeStamp - message.createdTimeStamp}ms. API latency is${Math.round(client.ping)}ms`);
-    });
-   
+    })
 }
 
 module.exports.help = {
