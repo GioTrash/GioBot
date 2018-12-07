4a03cd77ed01985a062d36ef647e526d40176c54
GioTrash <45596494+GioTrash@users.noreply.github.com>
2018-12-07 13:20:02 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -15,14 +15,14 @@ module.exports.run = async (bot, message, args) => {
     .setColor("RANDOM")
     .setThumbnail(message.author.avatarURL)
     .setTitle(`$user.username # ${user.discriminator}`)
-    .addField("Nickname :", `${member.nickname !== NULL ? `${member.nickname}` : "None"}`, true)
+    .addField("Nickname :", `${member.nickname !== null ? `${member.nickname}` : "None"}`, true)
     .addField("Created at :", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss ")}`, true)
     .addField("Bot :", `${user.bot}`, true)
     .addField("Status :", `${user.presence.status}`, true)
     .addField("Game :", `${user.presence.game ? user.presence.game.name : "None"}`, true)
-    .addField("Roles :", member.roles.map(roles => `${roles.name}`).join(', '), true)
+    .addField("Roles :", member.roles.map(roles => `${roles.name}`).join(', '), true);
 
-    message.channel.send({embed})
+    message.channel.send({embed});
       
 }
 
