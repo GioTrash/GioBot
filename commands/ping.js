61b563d73ff633a82117078f38b6a097b3a8908a
GioTrash <45596494+GioTrash@users.noreply.github.com>
2019-01-07 17:16:55 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async (bot, message, args) => {
-    const msg = await message.channel.send("ping?").then((msg) => {
+    const msg = await message.channel.send("ping?").then(({msg}) => {
         (`Pong! Latency is ${msg.createdTimeStamp - message.createdTimeStamp}ms. API latency is${Math.round(client.ping)}ms`);
     })
 }
